8d121d96ae2dada763ebfff9d365c15a7d7db24b
Tiangliang <578667035@qq.com>
2022-12-24 12:24:39 -0800

    add open buttion

--- src/pages/fileviewer.jsx
+++ src/pages/fileviewer.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { List, Typography, Button } from "antd";
 import html from "../api/config";
 
 export default function FileViewer() {
@@ -14,34 +15,51 @@ export default function FileViewer() {
       })
       .then((val) => {
         console.log(val);
-        setfilenames(val.data.children.map((name, id) => ({ name: name, id: id })));
+        setfilenames(
+          val.data.children.map((name, id) => ({ name: name, id: id }))
+        );
       });
-  },[directory_path]);
+  }, [directory_path]);
 
   const navigate = useNavigate();
   return (
     <>
-      <nav>
-        {filenames.length ? (
-          <>
-            <ul>
-              {filenames.map((filename) => (
-                <li key={filename.id}>{filename.name}</li>
-              ))}
-            </ul>
-          </>
-        ) : (
-          <>The Folder is empty</>
+      <List
+        header={<div>Path:{directory_path}</div>}
+        footer={
+          <div>
+            <button
+              type="button"
+              onClick={() => {
+                navigate(-1);
+              }}
+            >
+              Back
+            </button>
+          </div>
+        }
+        bordered
+        dataSource={filenames}
+        renderItem={(item) => (
+          <List.Item
+            actions={[
+              <Button
+                key="list-loadmore-edit"
+                onClick={() => {
+                  if (directory_path === "/")
+                    setdirectory_path(directory_path + item.name);
+                  else setdirectory_path(directory_path + "/" + item.name);
+                }}
+              >
+                open
+              </Button>,
+              <Button key="list-loadmore-more">delete</Button>,
+            ]}
+          >
+            <Typography.Text mark>[+]</Typography.Text> {item.name}
+          </List.Item>
         )}
-      </nav>
-      <button
-        type="button"
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        Back
-      </button>
+      />
     </>
   );
 }
